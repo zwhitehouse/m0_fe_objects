8041a72a02590e145a2150b6ce2917c92248dc14
Zack Whitehouse <zackwhitehouse225@gmail.com>
2023-08-07 16:32:00 -0400

    ADDED ANSWERS

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,27 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
+  eggs: 48,
+};
 
-var person = { height: "6 ft", weight: "160 lbs" }
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs" 
+};
 
-var post = { imageSrc: "./images/beach.png",
+var post = { 
+  imageSrc: './images/beach.png',
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+};
 
 
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+var post2 ={
+   imageSrc: './images/holiday-party.png',
+   caption: "What a great holiday party omg",
+   timestamp: "11:37 PM December 31, 2019",
+   numberLikes: 13,
+   comments: []
 }
