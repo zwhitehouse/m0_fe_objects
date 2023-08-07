1bbc09c8d036be0b2b85d56bef4a75dfaab0cc19
Zack Whitehouse <zackwhitehouse225@gmail.com>
2023-08-07 16:24:03 -0400

    Initial Commit

--- objects.js
+++ objects.js
@@ -15,17 +15,27 @@ var foods = {
 // you created above:
 // YOUR CODE HERE
 
+console.log(Object.keys(foods));
+
+
 // Write code that prints all of the 'values' of the foods variable 
 // you created above:
 // YOUR CODE HERE
+
+console.log(Object.values(foods));
 
 // Write code that prints the value of the second food of the foods variable 
 // you created above:
 // YOUR CODE HERE
 
+console.log(Object.values(foods)[1]);
+
 // Write code that adds a food to the foods object. 
 // Then, print the updated object:
 // YOUR CODE HERE
+
+foods.chips = 10;
+console.log(foods);
 
 
 // -------------------
@@ -39,17 +49,31 @@ var foods = {
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+var email = {
+  sender: 'Chris Erickson',
+  subject: 'Meeting today!',
+  timestamp: '10:34am',
+  date: 'Monday, August 5th',
+  forwarded: ['Keesha', 'Thomas', 'Jeff'],
+}
+
+
 // Write code that logs your email object to the terminal.
+
+console.log(email);
 
 
 // Write code that logs all of the 'keys' of the email object 
 // you created above:
 // YOUR CODE HERE
 
+console.log(Object.keys(email));
+
 // Write code that logs all of the 'values' of the email object 
 // you created above:
 // YOUR CODE HERE
 
+console.log(Object.values(email));
 
 // -------------------
 // Part 3: Many Emails - CHALLENGE!
@@ -99,3 +123,30 @@ console.log(posts[0]);
 // YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+var email = [
+{
+  sender: 'Chris Erickson',
+  subject: 'Meeting today!',
+  timestamp: '10:34am',
+  date: 'Monday, August 5th',
+  forwarded: ['Keesha', 'Thomas', 'Jeff'],
+},
+{
+  sender: 'Chris Erickson',
+  subject: 'Meeting today!',
+  timestamp: '10:34am',
+  date: 'Monday, August 5th',
+  forwarded: ['Keesha', 'Thomas', 'Jeff'],
+},
+{
+  sender: 'Chris Erickson',
+  subject: 'Meeting today!',
+  timestamp: '10:34am',
+  date: 'Monday, August 5th',
+  forwarded: ['Keesha', 'Thomas', 'Jeff'],
+},
+];
+
+console.log(email);
+
--- selfEval.md
+++ selfEval.md
@@ -1,5 +1,13 @@
 # Self-Evaluation
 
 - WThis final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+----It felt good to be able to take the challenge, try my best, make mistakes, fix those mistakes, and use resources to help complete the challenge successfully. I feel like it showed that I am learning as I could never have done any of this even a month ago.
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+My workflow is going alright, I do tend to get sucked into the work and attempt to complete problems without braking for hours which is taking a huge toll on my mental health as well as my ability to stay consistent and productive. I hope to change this as time goes on.
+
 - What did you learn from this exercise that you want to remember as you begin Mod 1?
+
+Honestly so many different things that I learned and would like to remember as I go into Mod 1. I know I won't remember everything, which is why it is important to know how to use google and other online resources to help when needed. If I had to choose a task to remember going into Mod 1 it would be that you can add new items to an already created object using the objectname.itemname = value. This seems like it could be very useful in many situations.
